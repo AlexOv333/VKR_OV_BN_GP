0cc7e0410fdbb225525f367964749b492460a16d
AlexOv333 <ovchinnikovalex2002@yandex.ru>
2024-02-27 10:39:55 +0300

    Переделанная топология, все ус-ва пингуют друг друга, на свитчах установлена BMv2

--- Топологии Mininet/my_topo.py
+++ Топологии Mininet/my_topo.py
@@ -1,30 +1,27 @@
-from mininet.net import Mininet
-from mininet.node import CPULimitedHost, RemoteController
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.cli import CLI
 from mininet.link import TCLink
 
-class MyTopology(Topo):
+class CustomTopology(Topo):
     def build(self):
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
+        #Входной узел
+        left_switch = self.addSwitch('s1')
+        #Выходной узел
+        right_switch = self.addSwitch('s2')
 
-        b1 = self.addSwitch('b1')
-        b2 = self.addSwitch('b2')
+        self.addLink(left_switch, right_switch)
 
-        self.addLink(h1, b1)
-        self.addLink(h2, b2)
-
+        
         for i in range(1, 11):
-            s = self.addSwitch('s%d' % i)
-            self.addLink(b1, s)
-            self.addLink(b2, s)
-            
+            host = self.addHost(f'h{i}')
+            self.addLink(host, left_switch)
+            self.addLink(host, right_switch)
 
-topo = MyTopology()
-
-net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink, controller=RemoteController, autoSetMacs=True)
-controller = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
-
-net.start()
-net.pingAll()
-net.interact()
+if __name__ == '__main__':
+    topo = CustomTopology()
+    net = Mininet(topo=topo, link=TCLink)
+    
+    net.start()
+    CLI(net)
+    net.stop()
